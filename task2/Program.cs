b10e9448dacf057a80bd96b789e909923679cd18
Dasha <dashabutenko@list.ru>
2022-12-12 23:36:12 +0400

    add task 2

---
+++ task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int sum = 0;
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(10);
+}
+
+PrintArray(array);
+
+for (int i = 1; i < array.Length; i = i + 2)
+{
+    sum = sum + array[i];
+}
+Console.Write($"Сумма элементов равна = {sum}\n");
+
+
+//-----------------------------
+void PrintArray(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write($"{array1[i]} ");
+    }
+    Console.WriteLine();
+}
